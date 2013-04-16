d92ca771b0e72160521540cad7fb857645bf358c
Emil Wall <emil.wall@valtech.se>
2013-04-16 10:18:57 +0100

    Initial Commit

---
+++ AFTest/EWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EWAppDelegate.h
+//  AFTest
+//
+//  Created by Emil Wall on 4/16/13.
+//  Copyright (c) 2013 Emil Wall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AFTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AFTest
+//
+//  Created by Emil Wall on 4/16/13.
+//  Copyright (c) 2013 Emil Wall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EWAppDelegate class]));
+    }
+}
