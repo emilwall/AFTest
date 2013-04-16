7b0cc4a8ad7854b7f342c54140a26ed9fee6f7b0
Emil Wall <emil.wall@valtech.se>
2013-04-16 11:07:35 +0100

    Add new class to contain API code

---
+++ AFTest/EWLastMinuteApi.h
@@ -0,0 +1,13 @@
+//
+//  EWLastMinuteApi.h
+//  AFTest
+//
+//  Created by Emil Wall on 4/16/13.
+//  Copyright (c) 2013 Emil Wall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EWLastMinuteApi : NSObject
+
+@end
---
+++ AFTest/EWLastMinuteApi.m
@@ -0,0 +1,13 @@
+//
+//  EWLastMinuteApi.m
+//  AFTest
+//
+//  Created by Emil Wall on 4/16/13.
+//  Copyright (c) 2013 Emil Wall. All rights reserved.
+//
+
+#import "EWLastMinuteApi.h"
+
+@implementation EWLastMinuteApi
+
+@end
